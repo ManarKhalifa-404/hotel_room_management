7054a5ba9c378766c588558a61337fcc609acb65
ManarKhalifa-404 <manarebrahim169@gmail.com>
2025-12-17 00:39:40 +0200

    Add sample room data for testing

--- lib/providers/room_provider.dart
+++ lib/providers/room_provider.dart
@@ -2,7 +2,31 @@ import 'package:flutter/material.dart';
 import '../models/room.dart';
 
 class RoomProvider with ChangeNotifier {
-  List<Room> _rooms = [];
+  List<Room> _rooms = [Room(
+      number: '101',
+      type: 'Single',
+      price: 100,
+      isavailable: true,
+    ),
+        Room(
+      number: '102',
+      type: 'Double',
+      price: 150,
+      isavailable: false,
+    ),
+    Room(
+      number: '103',
+      type: 'Suite',
+      price: 250,
+      isavailable: true,
+    ),
+      Room(
+      number: '104',
+      type: 'Single',
+      price: 90,
+      isavailable: false,
+    ),
+    ];
 
   List<Room> get rooms => _rooms;
 
